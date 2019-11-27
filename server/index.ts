4f1b0bacf8db4218bd682281ddd39a8a5e8c163d
Janet Milagros <janet.huacahuasi.garcia@gmail.com>
2019-11-27 09:09:07 -0500

    chore: graphql

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,10 @@
 import express from 'express';
-import next from 'next'
+import next from 'next';
+import morgan from 'morgan';
+
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
 
 const PORT = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -9,12 +14,47 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
     const server = express();
 
+    //settings
+    server.set('port', PORT);
+
+    //resolvers graphql
+    const createCliente = (id, {nombre}) => {
+        return {
+            id,
+            nombre
+        }
+    }
+    const clientesDB = {};
+    const root = {
+        cliente: () => {
+            return {
+                nombre: 'Diego'
+            }        
+        },
+        crearCliente: ({input}) => {
+            const id = require('crypto').randomBytes(10).toString('hex');
+            clientesDB[id] = input;
+            const client = createCliente(id, input);
+            return client;
+        }
+    }
+
+    //middlewares
+    server.use(morgan('dev'));
+    server.use('/graphql', graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true
+    }))
+
+
+    //routes
+
     server.all('*', (req: express.Request, res: express.Response) => {
         return handle(req, res)
       })
     
-    server.listen(PORT, () => {
-        // if (err) throw err
-        console.log(`> Ready on http://localhost:${PORT}`)
+    server.listen(server.get('port'), () => {
+        console.log(`> Ready on http://localhost:${server.get('port')}`)
     })
 })
---
+++ server/schema.ts
@@ -0,0 +1,33 @@
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+    type Cliente {
+        id: ID
+        nombre: String
+        apellido: String
+        empresa: String
+        email: String
+    }
+    
+    type Email {
+        email: String
+    }
+
+    type Query {
+        cliente: Cliente
+    }
+
+    input ClienteInput {
+        id: ID
+        nombre: String
+        // apellido: String
+        // empresa: String
+        // emails: String
+    }
+
+    type Mutation {
+        crearCliente(input: ClienteInput) : Cliente
+    }
+`);
+
+export default schema;
