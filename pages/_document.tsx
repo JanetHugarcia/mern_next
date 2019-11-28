7f724c6ed6bba479180ab6ab9b2ba32293052543
Janet Milagros <janet.huacahuasi.garcia@gmail.com>
2019-11-28 08:06:39 -0500

    chore(styles): se agrega material ui y styled-components

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const styledComponentSheet = new ServerStyleSheet();
+    const materialUiSheets = new MaterialUiServerStyleSheets()
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => 
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />),
+            )
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      styledComponentSheet.seal()
+    }
+  }
+}
